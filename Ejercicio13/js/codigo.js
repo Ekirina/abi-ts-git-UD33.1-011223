2f1720b24a51fb5b9ddd5c139852aa7999f782f6
Ainara <ainarabliz@gmail.com>
2023-12-02 20:13:20 +0100

    Falta 11 y 12

---
+++ Ejercicio13/js/codigo.js
@@ -0,0 +1,8 @@
+
+  function presionParrafoTabla1() {
+    alert('Se presionó un campo de la tabla1');
+  }
+  
+  function presionParrafoTabla2() {
+    alert('Se presionó un campo de la tabla2.');
+  }

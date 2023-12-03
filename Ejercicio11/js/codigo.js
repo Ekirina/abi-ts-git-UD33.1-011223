4a4cc05ae22e2f9449292f6adad34db882162c63
Ainara <ainarabliz@gmail.com>
2023-12-03 16:17:51 +0100

    done

--- Ejercicio11/js/codigo.js
+++ Ejercicio11/js/codigo.js
@@ -1,88 +1,59 @@
 var totalTiradas = 0;
-let array = [0,0,0,0,0,0,0];
-var cont = 0
+let array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var cont = 0;
 
-        do{ 
-            for (let index = 0; index < 36000; index++) {
-                dado1 = Math.floor(Math.random() * 6) + 1
-                dado2 = Math.floor(Math.random() * 6) + 1
-                cont++;                
-            }
-        
-        }while (cont <= 36000){
-                switch(dado1){
-                case 1:
-                    array[0]++;
-                    break;
-                case 2:
-                    array[1]++;
-                    break;
-                case 3:
-                    array[2]++;
-                    break;
-                case 4:
-                    array[3]++;
-                    break;
-                case 5:
-                    array[4]++;
-                    break;
-                case 6:
-                    array[5]++;
-                    break;
-                }
-            array[6] = cont;
-                switch (dado2) {
-                    case 1:
-                        array[0]++;
-                        break;
-                    case 2:
-                        array[1]++;
-                        break;
-                    case 3:
-                        array[2]++;
-                        break;
-                    case 4:
-                        array[3]++;
-                        break;
-                    case 5:
-                        array[4]++;
-                        break;
-                    case 6:
-                        array[5]++;
-                        break;
-                }
-            array[6] = cont;    
-        }
-        
-                
-        document.write("El 1 ha salido " +  [0] + " veces<br>");
-        document.write("El 2 ha salido " +  [1] + " veces<br>");
-        document.write("El 3 ha salido " +  [2] + " veces<br>");
-        document.write("El 4 ha salido " +  [3] + " veces<br>");
-        document.write("El 5 ha salido " +  [4] + " veces<br>");
-        document.write("El 6 ha salido " +  [5] + " veces<br>");
-        document.write("--------------------------------------");
-        document.write("Total de lanzamientos: " + [6] + (dado1+dado2));
+do {
+  dado1 = Math.random() * (6 - 1) + 1;
+  dado2 = Math.random() * (6 - 1) + 1;
+  
+  suma = dado1+dado2;
+  cont++;
+  switch (suma) {
+    case 2:
+      array[0]++;
+      break;
+    case 3:
+      array[1]++;
+      break;
+    case 4:
+      array[2]++;
+      break;
+    case 5:
+      array[3]++;
+      break;
+    case 6:
+      array[4]++;
+      break;
+    case 7:
+      array[5]++;
+      break;
+    case 8:
+      array[6]++;
+      break;
+    case 9:
+      array[7]++;
+      break;
+    case 10:
+      array[8]++;
+      break;
+    case 11:
+      array[9]++;
+      break;
+    case 12:
+      array[10]++;
+      break; 
+  }
+} while (cont <= 36000);
 
- /*       // Inicializar array para contar las sumas
-let sumasArray = new Array(11).fill(0); // Las sumas posibles van de 2 a 12
+document.write("El 2 ha salido " + [0] + " veces<br>");
+document.write("El 3 ha salido " + [1] + " veces<br>");
+document.write("El 4 ha salido " + [2] + " veces<br>");
+document.write("El 5 ha salido " + [3] + " veces<br>");
+document.write("El 6 ha salido " + [4] + " veces<br>");
+document.write("El 7 ha salido " + [5] + " veces<br>");
+document.write("El 8 ha salido " + [0] + " veces<br>");
+document.write("El 9 ha salido " + [1] + " veces<br>");
+document.write("El 10 ha salido " + [2] + " veces<br>");
+document.write("El 11 ha salido " + [3] + " veces<br>");
+document.write("El 12 ha salido " + [4] + " veces<br>");
 
-// Simular el lanzamiento de dos dados 36000 veces
-for (let i = 0; i < 36000; i++) {
-    // Lanzamiento de dos dados
-    let dado1 = Math.floor(Math.random() * 6) + 1;
-    let dado2 = Math.floor(Math.random() * 6) + 1;
-
-    // Calcular la suma de los dados
-    let suma = dado1 + dado2;
-
-    // Contar las apariciones de la suma
-    sumasArray[suma - 2]++; // Restamos 2 ya que las sumas posibles van de 2 a 12
-}
-
-// Imprimir los resultados
-for (let i = 2; i <= 12; i++) {
-    console.log(`La suma ${i} ha aparecido ${sumasArray[i - 2]} veces.`);
-}*/
-
-        
